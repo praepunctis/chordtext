292feea0d56336cf3b2635ed8d9dd0b67228894d
Kelly Fesler <kellyfesler@gmail.com>
2020-11-14 04:08:35 -0800

    flaskapp: set up barebones app

--- flaskapp.py
+++ flaskapp.py
@@ -1,3 +1,14 @@
 # flaskapp.py
 # Kelly Fesler (c) Nov 2020
 # Modified from Soumya Gupta (c) Jan 2020
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
